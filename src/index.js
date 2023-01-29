ae53292d9d824842daf448154f3ffea7f5d9d64d
davifdepaula <davi.ferreira100@gmail.com>
2023-01-29 08:08:57 -0300

    feat: product/productdetail

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import router from './routes/routes.js';
+import cors from 'cors'
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
 app.use(router);
 
-app.listen(5000, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,15 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import auth from '../middlewares/AuthMiddleware.js';
 import { register, authenticate } from '../controllers/AuthController.js';
+import { getProducts, getProduct } from '../controllers/Products.js';
 
 const router = express.Router();
-
 router.use(cors());
 router.use(express.json());
-
+router.get('/produtos', getProducts)
+router.get('/produtos/:id', getProduct)
 router.post('/register', register);
 router.post('/authenticate', authenticate);
-
 router.use(auth);
 
 export default router;
